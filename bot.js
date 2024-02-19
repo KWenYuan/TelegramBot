c1c064e16368de8da9099499fe40a03e528d8969
KWenYuan <93873311+KWenYuan@users.noreply.github.com>
2024-02-19 23:21:22 +0800

    Updated the first one to be calendar instead

--- bot.js
+++ bot.js
@@ -1,21 +1,32 @@
 const { Telegraf, Scenes, session, Markup } = require('telegraf');
 const Calendar = require('telegraf-calendar-telegram');
-const calendar = new Calendar(bot);
+
 
 // Create a new instance of the Telegraf bot
 const bot = new Telegraf('5805295303:AAGHdT0oLQPVghoWZ7_oo1hGM4bVDus689c');
+const calendar = new Calendar(bot);
 
 const questionScene = new Scenes.BaseScene('QUESTION_SCENE');
 
-const questions = ['What is the date?', 'What is the time?', 'What is the venue?', 'How much was spent?',
+const questions = ['What is the time?', 'What is the venue?', 'How much was spent?',
 'What is the name of the client?', 'Where did the lead come from?', 'What are the notes about the client?',
 'What is the client\'s age?', 'What is the client\'s marital status?', 'How many dependents does the client have?',
 'What is the client\'s occupation?', 'What is the client\'s annual income?', 'What was sold to the client?',
-'What was proposed to the client?', 'When is the next appointment?'];
+'What was proposed to the client?', 'What is the date of the next appointment?'];
+let date = [];
 let answers = [];
 let notes = [];
 
 questionScene.enter((ctx) => {
+    ctx.reply('What is the date?', calendar.getCalendar());
+});
+
+bot.action(/.*/, (ctx) => {
+    ctx.answerCbQuery();
+    const dateComponents = ctx.update.callback_query.data.split('-').slice(2);
+    dateComponents.shift();
+    const formattedDate = dateComponents.reverse().join('-');
+    date.push(formattedDate);
     ctx.reply(questions[0]);
 });
 
@@ -39,24 +50,24 @@ questionScene.on('text', (ctx) => {
             ctx.reply(
                 `*Notes After Appointment*
                 
-*Date:* ${answers[0]}
-*Time:* ${answers[1]}
-*Venue:* ${answers[2]}
-*Amount Spent:* ${answers[3]}
+*Date:* ${date[0]}
+*Time:* ${answers[0]}
+*Venue:* ${answers[1]}
+*Amount Spent:* ${answers[2]}
                 
-*Name:* ${answers[4]}
-*Leads from:* ${answers[5]}
+*Name:* ${answers[3]}
+*Leads from:* ${answers[4]}
 *Notes about client (mainly what was discussed or talked about during the appointment):*
-${answers[6]}
+${answers[5]}
                 
-*Client age:* ${answers[7]}
-*Marital Status:* ${answers[8]}
-*Dependents:*  ${answers[9]}
-*Occupation:* ${answers[10]}
-*Annual Income:* ${answers[11]}
-*What was sold:* ${answers[12]}
-*What was proposed:* ${answers[13]}
-*Date of Next Appt:* ${answers[14]}`,
+*Client age:* ${answers[6]}
+*Marital Status:* ${answers[78]}
+*Dependents:*  ${answers[8]}
+*Occupation:* ${answers[9]}
+*Annual Income:* ${answers[10]}
+*What was sold:* ${answers[11]}
+*What was proposed:* ${answers[12]}
+*Date of Next Appt:* ${answers[13]}`,
 { parse_mode: 'Markdown' }
 );
             answers = []; // Reset answers
