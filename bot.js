26938d5fbd217cf22b8532d3cc97bd2433d65ff3
KWenYuan <93873311+KWenYuan@users.noreply.github.com>
2024-02-19 22:49:59 +0800

    Updated

--- bot.js
+++ bot.js
@@ -1,35 +1,9 @@
 const { Telegraf, Scenes, session, Markup } = require('telegraf');
+const Calendar = require('telegraf-calendar-telegram');
+const calendar = new Calendar(bot);
 
 // Create a new instance of the Telegraf bot
 const bot = new Telegraf('5805295303:AAGHdT0oLQPVghoWZ7_oo1hGM4bVDus689c');
-
-// Register a command handler
-// bot.command('start', (ctx) => {
-//     ctx.reply('Hello! Welcome to our customer service bot. How can we assist you today?', 
-//         Markup.inlineKeyboard([
-//             Markup.button.callback('Order Inquiry', 'order'),
-//             Markup.button.callback('Refund Request', 'refund'),
-//             Markup.button.callback('Shipping Inquiry', 'shipping')
-//         ])
-//     );
-// });
-
-// // Register callback query handler
-// bot.action('order', async (ctx) => {
-//     await ctx.editMessageReplyMarkup({});
-//     ctx.reply('Please provide your order number so we can assist you further.');
-// });
-
-// bot.action('refund', async (ctx) => {
-//     await ctx.editMessageReplyMarkup({});
-//     ctx.reply('To request a refund, please visit our website and fill out the refund form.');
-// });
-
-// bot.action('shipping', async (ctx) => {
-//     await ctx.editMessageReplyMarkup({});
-//     ctx.reply('For shipping inquiries, please contact our support team at support@example.com.');
-// });
-
 
 const questionScene = new Scenes.BaseScene('QUESTION_SCENE');
 
